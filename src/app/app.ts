77c98af4dd04f8d33ea327d4fd134f8624d731e6
Neven <nevzurcher@gmail.com>
2025-08-13 12:24:34 -0400

    Day 1 - Initial Angular app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,17 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component} from '@angular/core';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  standalone: true,
+  imports: [NgFor]
 })
 export class App {
-  protected readonly title = signal('finance-dashboard');
+  title = 'Finance Dashboard';
+  accounts = [
+    { name: 'Checking', balance: 1200 },
+    { name: 'Savings', balance: 5400}
+  ];
 }
